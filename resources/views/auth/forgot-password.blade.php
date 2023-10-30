45fc4433b0a579d2c0b6194a72becd7485887ed7
Daníla Temoteo Lucas <80535640+danilalucas@users.noreply.github.com>
2023-10-30 20:19:12 -0300

    Style auth screens

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -3,24 +3,38 @@
 @section('content')
 <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
     <div class="col-md-6">
+        <div class="row">
+            <div class="col text-center text-info">
+                <h4 class="p-2"> {{ config('app.name') }} </h4>
+            </div>
+        </div>
+        
         <div class="card">
-            <div class="card-header">{{ __('Forgot Password') }}</div>
+            <div class="card-header bg-primary text-white"> {{ __('Forgot Password') }} </div>
             <div class="card-body">
                 @if ($errors->any())
                     <div class="alert alert-danger" role="alert">
                         {{ __('It was not possible to send the email. Try again') }}
-                    </div>
-                @else
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>               
+                @endif
+
+                @if (session('status'))
                     <div class="alert alert-success" role="alert">
-                        {{ __('We have emailed your password reset link') }}
-                    </div>                
+                        {{ __(session('status')) }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
                 @endif
 
                 <form method="POST" action="{{ route('password.email') }}">
                     @csrf
 
                     <div class="form-group">
-                        <label for="email">{{ __('Email') }}</label>
+                        <label for="email"> {{ __('Email') }} </label>
                         <input id="email" type="email" name="email" class="form-control" required autofocus>
                     </div>
                     <br>
@@ -30,7 +44,7 @@
                         </button>
                     </div>
                     <div class="mt-3 text-center">
-                        <a href="{{ route('login') }}">{{ __('Back to Login') }}</a>
+                        <a href="{{ route('login') }}"> {{ __('Back to Login') }} </a>
                     </div>
                 </form>
             </div>
